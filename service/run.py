afffecdc9693a39e5100808b27737affa1ed95f4
Silviu Panica <silviu.panica@e-uvt.ro>
2023-01-25 15:40:04 +0200

    added k8s deployment files

--- service/run.py
+++ service/run.py
@@ -1,19 +1,22 @@
-
-
+import os
 from ede_service import *
+
 if __name__ == '__main__':
     host = os.environ.get('EDE_HOST', '0.0.0.0')
     port = os.environ.get('EDE_PORT', 5001)
     debug = os.environ.get('EDE_DEBUG', True)
+    log_location = os.environ.get('EDE_LOG_LOCATION', os.path.dirname(os.path.abspath(__file__)))
     if __name__ == '__main__':
         try:
             import bjorn
             bjorn.run(
                 host=host,
                 port=port,
-                debug=debug)
+                debug=debug,
+                log_location=log_location)
         except ImportError:
             app.run(
                 host=host,
                 port=port,
-                debug=debug)
+                debug=debug,
+                log_location=log_location)
